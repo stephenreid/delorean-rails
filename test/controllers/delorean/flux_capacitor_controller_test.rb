910775c6892936e9594ad6216e4c1cd8533e0a8e
Stephen Reid <stephen.reid@terminus.com>
2017-03-01 16:30:34 -0500

    Testing

--- test/controllers/delorean/flux_capacitor_controller_test.rb
+++ test/controllers/delorean/flux_capacitor_controller_test.rb
@@ -6,9 +6,34 @@ module Delorean
       @routes = Engine.routes
     end
 
-    test 'shows index' do
+    teardown do
+      Timecop.return
+    end
+
+    test 'GET #index' do
       get :index
       assert_response :success
+    end
+
+    test 'POST #start' do
+      post :start
+      assert_in_delta Time.now, session[:delorean_start_time], 1.seconds
+    end
+
+    test 'POST #flux' do
+      current_time = Time.now
+      post :flux, { scale: 1000 }
+      sleep(0.25)
+      assert_response :success
+      assert_equal 1000, session[:delorean_scale].to_i
+      refute_in_delta Time.now, current_time, 5.seconds
+    end
+
+    test 'POST #accelerate_to_eighty_eight' do
+      post :flux, { date: { year: 2016, month: 1, day: 30, hour: 1, minute: 11 } }
+      assert_response :success
+      assert_equal Time.new("2016-01-30 1:11 -0500"), Time.new(session[:delorean_future])
+      assert_in_delta Time.now, 2.days.ago, 1.seconds
     end
   end
 end
